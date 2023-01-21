c64c00d34b488e1e5c07a05c808a973fedde3be1
AllaamRosyad <allaamrosyadakbar@gmail.com>
2023-01-21 18:49:24 +0700

    Vue3

--- Vue3/src/App.vue
+++ Vue3/src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+//tempat ngoding javascript
+
+export default{
+  data(){
+    return{
+      datasensor: "60",
+      datasensor2: "80"
+    };
+  },
+  created(){
+    //this.getData();
+  },
+  methods: {
+    async getData() {
+      const data = await fetch("http://localhost:3000/sensor1");
+      const datajson = await data.json()
+      this.datasensor = datajson.sensor1
+    }
+  },
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!--tempat ngoding html-->
+  <div>Hello {{ datasensor }}
+  <button @click="getData">Ambil Data</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 </style>
